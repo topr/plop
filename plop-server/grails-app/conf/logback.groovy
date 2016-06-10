08d20f0cf1a7568daa5e8410e9f0f223020d0aed
Tomasz Przybysz <tomasz.przybysz@gmail.com>
2016-06-10 22:46:55 +0100

    (trivial) clean logback a bit

--- plop-server/grails-app/conf/logback.groovy
+++ plop-server/grails-app/conf/logback.groovy
@@ -4,20 +4,20 @@ import grails.util.Environment
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
+        pattern = '%level %logger - %msg%n'
     }
 }
 
 root(ERROR, ['STDOUT'])
 
 def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir) {
-    appender("FULL_STACKTRACE", FileAppender) {
+if (Environment.developmentMode && targetDir) {
+    appender('FULL_STACKTRACE', FileAppender) {
         file = "${targetDir}/stacktrace.log"
         append = true
         encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
+            pattern = '%level %logger - %msg%n'
         }
     }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+    logger('StackTrace', ERROR, ['FULL_STACKTRACE'], false)
 }
