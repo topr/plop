5ebe5818e6af74afe8f52c36679c58d857c75915
Tomasz Przybysz <tomasz.przybysz@gmail.com>
2016-07-24 21:44:21 +0100

    webpack automatic entries resolution

--- plop-web/src/main/javascript/example/amd-module/app.js
+++ plop-web/src/main/javascript/example/amd-module/app.js
@@ -1,4 +1,4 @@
-import '../../../css/style.css';
+import './style.css';
 
 import displayTime from './displayTime';
 
--- plop-web/webpack.config.js
+++ plop-web/webpack.config.js
@@ -1,11 +1,33 @@
+const readDir = require('fs-readdir-recursive');
+const path = require('path');
+const webpack = require('webpack');
+const failPlugin = require('webpack-fail-plugin');
+
+const VERSION = require('./package.json').version;
+const SOURCES_DIR = `${__dirname}/src/main`;
+const ENTRY_REGEXP = new RegExp('.*?app\.[a-z]+$');
+
+const isEntryPath = (filePath) => ENTRY_REGEXP.test(filePath);
+const createEntry = (entries, path) => {
+
+    var pathParts = path.split('/');
+    var name = pathParts.slice(1, pathParts.length - 1).join('.');
+
+    console.log(`Entry: ${name}`);
+    entries[name] = `${SOURCES_DIR}/${path}`;
+
+    return entries;
+};
+
 module.exports = {
     context: __dirname,
-    entry: {
-        'amd-module.example': './src/main/javascript/example/amd-module/app.js'
-    },
+    entry: readDir(SOURCES_DIR)
+        .filter(isEntryPath)
+        .reduce(createEntry, {}),
     output: {
-        path: './build',
-        filename: '[name].js'
+        path: path.resolve(__dirname, 'build'),
+        filename: `[name]-${VERSION}.js`,
+        publicPath: '/assets/',
     },
     devtool: 'source-map',
     module: {
@@ -26,6 +48,7 @@ module.exports = {
         ]
     },
     plugins: [
-        require('webpack-fail-plugin')
+        failPlugin,
+        new webpack.DefinePlugin({VERSION})
     ]
 };
